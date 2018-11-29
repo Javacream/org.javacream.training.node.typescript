662223f639dae02d97919405b4c39974b5ccf566
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-11-29 14:41:36 +0100

    add demo

---
+++ ts/demo.ts
@@ -0,0 +1,28 @@
+namespace BasicTypes{
+    namespace SimpleDeclaration{
+      let name:string = "Hugo"
+      let state:boolean = true
+      let number:number = 42
+      console.log(number)
+    }
+  
+    namespace Declarations{
+      const name:string = "42"
+      let state:boolean = true
+      state = false
+    }
+  
+    namespace TypeAnnotations{
+      let value :any ="Hello"
+      const message :string = <string>value
+      const message2 : string = value as string
+    }
+  
+    namespace Namespace1{
+      export let message = "Hello from namespace1"
+    }
+  
+    namespace Namespace2{
+      console.log(Namespace1.message);
+    }
+  }
