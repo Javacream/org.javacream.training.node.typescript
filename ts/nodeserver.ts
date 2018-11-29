7171878c3cfc41f21c58b498b6191c84b1d3555f
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-11-29 15:29:34 +0100

    add classes

--- ts/nodeserver.ts
+++ ts/nodeserver.ts
@@ -4,7 +4,7 @@ function demo(){
     http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World!\n');
-    }).listen(8124);
+    }).listen(8125);
     console.log('Server running at http://127.0.0.1:8125/');
 
   }
